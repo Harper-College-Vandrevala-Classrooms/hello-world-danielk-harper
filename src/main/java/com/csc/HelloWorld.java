61803ff3e3cf653ce76b761dce6a23203323fcee
danielk-harper <d_kaeseberg1@mail.harpercollege.edu>
2024-08-25 21:50:48 -0500

    Create HelloWorld class.
    Created the HelloWorld class with the entry point that prints "Hello World!".

--- src/main/java/com/csc/HelloWorld.java
+++ src/main/java/com/csc/HelloWorld.java
@@ -1,3 +1,10 @@
 package com.csc;
 
+import java.io.*;
+
 // Your code goes here!
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!\n");
+    }
+}
